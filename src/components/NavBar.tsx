ad9b3f9f2715fd41168ddb4d52e272dc0d215100
sandunrmst <sandunalugc@gmail.com>
2023-09-25 16:41:45 +0530

    fetch Games from API

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/playMatrix.webp";
-import ColorModeSwitch from "./colorModeSwitch";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
