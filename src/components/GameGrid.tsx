133198737984b208e9d82f04dd906c0b9382232c
sandunrmst <sandunalugc@gmail.com>
2023-10-01 11:46:52 +0530

    Display genre list and fix UI issues

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,29 +1,9 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGame from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkelton from "./GameCardSkelton";
 import useGames from "../hooks/useGames";
 
-// interface Game {
-//   id: number;
-//   name: string;
-// }
-
-// interface FetchGamesData {
-//   count: number;
-//   results: Game[];
-// }
-
 const GameGrid = () => {
-  //   const [games, setGames] = useState<Game[]>([]);
-  //   const [error, setError] = useState("");
-
-  //   useEffect(() => {
-  //     apiClient
-  //       .get<FetchGamesData>("/games")
-  //       .then((res) => setGames(res.data.results))
-  //       .catch((err) => setError(err.message));
-  //   }, []);
   const { data, error, isLoading } = useGames(); //Get data from the custom hook
   const Skeletons = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
@@ -34,7 +14,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding={10}
-        spacing={10}
+        spacing={6}
       >
         {isLoading &&
           Skeletons.map((skeleton) => <GameCardSkelton key={skeleton} />)}
