c6a2cf1e3b4462bd89dab4d11a6b5b639390c0c4
sandunrmst <sandunalugc@gmail.com>
2023-10-03 15:56:37 +0530

    finalize the playMatrix app

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,21 +17,21 @@ const GameGrid = ({
   const Skeletons = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
+
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding={10}
-        spacing={6}
-      >
-        {isLoading &&
-          Skeletons.map((skeleton) => <GameCardSkelton key={skeleton} />)}
-        {data.map((game) => (
-          <GameCard key={game.id} game={game} />
-        ))}
-      </SimpleGrid>
-    </>
+    /* {error && <Text>{error}</Text>} */
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+      padding={10}
+      spacing={6}
+    >
+      {isLoading &&
+        Skeletons.map((skeleton) => <GameCardSkelton key={skeleton} />)}
+      {data.map((game) => (
+        <GameCard key={game.id} game={game} />
+      ))}
+    </SimpleGrid>
   );
 };
 
